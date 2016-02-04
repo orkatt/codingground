30b24de697fee192b935163953759bd5c3abbf2f
orkatt <Alexander.E.Maklakov@gmail.com>
2016-02-05 02:35:57 +0300

    Update main.hs

--- Tenses/main.hs
+++ Tenses/main.hs
@@ -2,10 +2,13 @@ continues :: String -> String
 continues "be" = "being"
 continues verb = verb++"ing"
 
+parse :: (String, String) -> [String]
+parse (i, v) = [""]
+
 main = do
 {
     putStrLn "hello world";
-    x <- getLine;
-
-    putStrLn (continues x);
+    y <- fmap words getLine;
+    
+    putStrLn parse (head y, last y);
 }
