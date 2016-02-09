5088e2c0bfdd8946ea0acd797768461ca38e8554
orkatt <Alexander.E.Maklakov@gmail.com>
2016-02-10 02:26:53 +0300

    Update main.hs

--- Tenses/main.hs
+++ Tenses/main.hs
@@ -2,13 +2,42 @@ continues :: String -> String
 continues "be" = "being"
 continues verb = verb++"ing"
 
-parse :: (String, String) -> [String]
-parse (i, v) = [""]
+contW :: String -> String
+contW "I" = "am"
+contW "You" = "are"
+contW "He" = "is"
+contW "She" = "is"
+contW "It" = "is"
+
+
+tenseCont :: [String] -> [String]
+tenseCont (who:verb) =
+      who : 
+      contW who : 
+      continues (head verb) : 
+      tail verb
+      
+
+
+printLs :: [String] -> IO ()
+printLs [] = do putStr ""
+printLs s = do
+    putStr (head s)
+    if length s > 1 then putStr " " else putStr ""
+    printLs (tail s)
+
+
+printL :: [String] -> IO ()
+printL s = do
+   putStr "-<[ "
+   printLs s
+   putStr " ]>-"
+
 
 main = do
 {
     putStrLn "hello world";
     y <- fmap words getLine;
     
-    putStrLn parse (head y, last y);
+    printL (tenseCont y)
 }
